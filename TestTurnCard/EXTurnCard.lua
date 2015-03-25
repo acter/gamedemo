684f6185e7789803c28d06bd852b5dc0f7b11c33
leexg <actergame@gmail.com>
2015-03-25 13:15:20 +0800

    fix
    fix

--- TestTurnCard/EXTurnCard.lua
+++ TestTurnCard/EXTurnCard.lua
@@ -22,21 +22,19 @@ function EXTurnCard:create(inCardImageName,outCardImageName,duration)
     openAnimIn = cc.Sequence:create(cc.DelayTime:create(duration/5),cc.Show:create(),cc.OrbitCamera:create(duration/5, 1, 0, kInAngleZ, kInAngleZ, 0, 0))
 
     openAnimIn:retain()
---    openAnimOut = cc.Sequence:create(cc.OrbitCamera:create(duration/5, 1, 0, kInAngleZ, kInAngleZ, 0, 0),cc.Hide:create(),cc.DelayTime:create(duration/5))
-    openAnimOut = cc.Sequence:create(cc.Hide:create(),cc.DelayTime:create(duration),cc.Show:create())
-    
+   openAnimOut = cc.Sequence:create(cc.OrbitCamera:create(duration/5, 1, 0, kInAngleZ, kInAngleZ, 0, 0),cc.Hide:create(),cc.DelayTime:create(duration/5))    
    
     openAnimOut:retain()
     layer.openCard = function() 
---    	 inCard:runAction(openAnimIn)
-		 outCard:runAction(openAnimOut)
+        inCard:runAction(openAnimIn)
+        outCard:runAction(openAnimOut)
     end
     return layer
 end
 
 function EXTurnCard:openCard()
-	self.inCard:runAction(openAnimIn)
-	self.outCard:runAction(openAnimOut)
+    self.inCard:runAction(openAnimIn)
+    self.outCard:runAction(openAnimOut)
 end
 
 return EXTurnCard
